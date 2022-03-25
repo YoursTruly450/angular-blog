f5641cdccf84b41a7112776029412c77a12e398f
meluferiev <meluferiev@axbit.ru>
2022-03-25 17:38:47 +0400

    feat: create posts list

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Post } from 'src/app/shared/interfaces';
+import { PostService } from 'src/app/shared/post.service';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  posts: Post[] = [];
+  pSub: Subscription
+
+  constructor(
+    private postService: PostService
+  ) { }
 
   ngOnInit(): void {
+    this.getAllPosts();
   }
+
+  ngOnDestroy(): void {
+    if (this.pSub) {
+      this.pSub.unsubscribe();
+    }
+  }
+
+  getAllPosts() {
+    this.pSub = this.postService.getAll().subscribe(
+      (response) => {
+        this.posts = response;
+      }
+    );
+  }
+
+  remove(id: string) {}
 
 }
