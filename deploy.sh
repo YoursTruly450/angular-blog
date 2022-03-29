b68b2afa77d9682ee180da0c308bddf8cc307e10
YoursTruly450 <yourstruly.bill@gmail.com>
2022-03-29 22:05:15 +0400

    feat: add deploy sh

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ cd angular-blog
 git init
 git add -A
 git commit -m "New Deployment"
-git push -f git@github.com:YoursTruly450/angular-blog.git main:gh-pages
+git push -f git@github.com:YoursTruly450/angular-blog.git master:gh-pages
 
 cd -
